3c04a32eb65c2c64812d749a78630ff638d13fd0
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-12-13 22:44:59 +0800

    checked for non number input

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 import random
 
-class NubmberGG:
+class NumberGG:
     def __init__(self):
-        self.intro = """
-Welcome to number-g-g aka number guessing game.
-I am thinking a number between 1 and 100
+        self.gained_chances = 0
+        self.current_chances = 0
+        self.the_number = random.randint(1,100)
+        self.player_guess = 0
+    def intro(self):
+        result = """
+        Welcome to number guessing game aka number-g-g.
+        I am thinking a number between 1 and 100
 
-Please select a difficulty level.
-1. Easy -> 10 chances
-2. Medium -> 5 chances
-3. Hard -> 3 chances
-"""
-        print(self.intro)
-        self.current_chance = 0
-        self.number_to_guess = random.randint(1,100)
+        Select your levels:
+        1. Easy (10 chances)
+        2. Medium (5 chances)
+        3. Hard (3 chances)
+        """
+        return result
+
+    def get_chances(self, level):
+        chances = [10,5,3]
+        self.gained_chances = chances[level-1]
 
 
-    def check_player_number(self, number):
-        if self.number_to_guess > number:
-            print(f"The number is greater than {number}")
-        elif self.number_to_guess < number:
-            print(f"The number is less than {number}")
-        elif self.number_to_guess == number:
-            print(f"Congratulations you guessed the number in {self.current_chance} attempts.")
+    def check_number(self,number):
+        if self.current_chances == self.gained_chances:
+            print(f"Game over. The number is {self.the_number}({self.current_chances} attempts).")
+        elif number > self.the_number:
+            print(f"{number} is greater than the number({self.current_chances} attempts).")
+        elif number < self.the_number:
+            print(f"{number} is less than the number({self.current_chances} attempts).")
+        elif number == self.the_number:
+            print(f"Congratutions. The number is {self.the_number}({self.current_chances} attempts).")
 
 
     def start_game(self):
-        """Function: for starting game"""
+        print(self.intro())
+        while True:
+            user_level = int(input("Enter level here: "))
+            self.get_chances(user_level)
+            if user_level in [1,2,3]:
+                break
+            else:
+                print('invalid level.')
 
-        # checks user input is a correct level
         while True:
-            selected_chances = int(input("Enter here:"))
-            if selected_chances in [3,5,10]:
-                break
-        
-        # run the game loop
-        while True:
-            self.current_chance += 1
-            player_number = int(input("Enter your guess: "))
-            self.check_player_number(player_number)
-            if self.current_chance == selected_chances:
-                print(f"Game over. The number is {self.number_to_guess}.")
-                break
-            elif player_number == self.number_to_guess:
-                break
+            try:
+                self.player_guess = int(input("What's the number? "))
+                if self.gained_chances == self.current_chances:
+                    self.check_number(self.player_guess)
+                    break
+                elif self.player_guess == self.the_number:
+                    self.check_number(self.player_guess)
+                    break
+                else:
+                    self.current_chances += 1 
+                    self.check_number(self.player_guess)
+            except Exception:
+                print("Not a number.")
+
+
 
 def main():
-    game = NubmberGG()
+    game = NumberGG()
     game.start_game()
-
 
 if __name__ == "__main__":
     main()
+
